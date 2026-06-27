1284841c481d91ba5559811736efd2e0d9649bdc
ark2242 <zwf471952145@outlook.com>
2026-06-27 21:39:24 +0800

    Some adds

--- app/src/main/java/com/qx/orbit/bili/data/api/TestOkHttp.kt
+++
@@ -1,6 +0,0 @@
-package com.qx.orbit.bili.data.api
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody.Companion.toRequestBody
-fun test() {
-    val b = "{}".toRequestBody("application/json".toMediaTypeOrNull())
-}
